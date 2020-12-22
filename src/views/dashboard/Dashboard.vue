5030d31b0912cab74b215b83a209727db750b66f
ByteInternetHK <byteinternethk@gmail.com>
2020-12-22 18:23:37 +0800

    update admin dashboard

--- src/views/dashboard/Dashboard.vue
+++ src/views/dashboard/Dashboard.vue
@@ -12,39 +12,70 @@
         </a-col>
         <a-col :sm="24" :md="12" :xl="6" :style="{ marginBottom: '24px' }">
           <a-card :title="$i18n.t('dashboard.info.node')" :bordered="false" style="height: 181px">
-            <span style="font-size: 14px;font-weight: bold">{{ $i18n.t('dashboard.info.nodeCount') }} : </span> <span style="font-size: 18px;font-weight: bold;">
-              <router-link :to="{path: '/admin/node'}">{{ dashboard.nodeCount }}</router-link>
+            <span>
+              <span>{{ $i18n.t('dashboard.info.nodeCount') }} : </span>
+              <span style="font-size: 18px;font-weight: bold;">
+                <router-link :to="{path: '/admin/node'}">{{ dashboard.nodeCount }}</router-link>
+              </span>
+              <br>
             </span>
-            <p style="font-size: 16px;font-weight: bold;margin-top: 20px">{{ $i18n.t('dashboard.info.offlineCount') }} : <span style="font-size: 24px;font-weight: bold;">{{ dashboard.offlineCount }}</span></p>
+            <span>
+              <span>{{ $i18n.t('dashboard.info.offlineCount') }} : </span>
+              <span style="font-size: 18px;font-weight: bold;">{{ dashboard.offlineCount }}</span>
+              <br>
+            </span>
           </a-card>
         </a-col>
         <a-col :sm="24" :md="12":xl="6" :style="{ marginBottom: '24px' }">
           <a-card :title="$i18n.t('dashboard.info.user')" :bordered="false" style="height: 181px">
-            <span style="font-size: 14px;font-weight: bold">{{ $i18n.t('dashboard.info.userCount') }} : </span> <span style="font-size: 18px;font-weight: bold;">
-              <router-link :to="{path: '/admin/user'}">{{ dashboard.userCount }}</router-link>
+            <span>
+              <span>{{ $i18n.t('dashboard.info.userCount') }} : </span>
+              <span style="font-size: 18px;font-weight: bold;">
+                <router-link :to="{path: '/admin/user'}">{{ dashboard.userCount }}</router-link>
+              </span>
+              <br>
             </span>
-            <p style="font-size: 16px;font-weight: bold;margin-top: 20px">{{ $i18n.t('dashboard.info.todayRegisterCount') }} : <span style="font-size: 24px;font-weight: bold;">{{ dashboard.todayRegisterCount }}</span></p>
+            <span>
+              <span>{{ $i18n.t('dashboard.info.monthRegisterCount') }} : </span>
+              <span style="font-size: 18px;font-weight: bold;">{{ dashboard.monthRegisterCount }}</span>
+              <br>
+            </span>
+            <span>
+              <span>{{ $i18n.t('dashboard.info.todayRegisterCount') }} : </span>
+              <span style="font-size: 18px;font-weight: bold;">{{ dashboard.todayRegisterCount }}</span>
+              <br>
+            </span>
           </a-card>
         </a-col>
         <a-col :sm="24" :md="12" :xl="6" :style="{ marginBottom: '24px' }">
           <a-card :title="$i18n.t('dashboard.info.income')" :bordered="false" style="height: 181px">
-            <span style="font-size: 14px;font-weight: bold">{{ $i18n.t('dashboard.info.monthIncome') }} : </span> <span style="font-size: 18px;font-weight: bold;">{{ dashboard.monthIncome }}</span>
-            <p style="font-size: 16px;font-weight: bold;margin-top: 20px">
-              {{ $i18n.t('dashboard.info.todayIncome') }} : <span style="font-size: 24px;font-weight: bold;">{{ dashboard.todayIncome }}</span>
-              <span style="margin-left: 8px">{{ $i18n.t('dashboard.info.todayNewOrderCount') }}:{{ dashboard.todayNewOrderCount }} </span>
-            </p>
+            <span>
+              <span>{{ $i18n.t('dashboard.info.monthIncome') }} : </span>
+              <span style="font-size: 18px;font-weight: bold;">{{ dashboard.monthIncome }}</span>
+              <br>
+            </span>
+            <span>
+              <span>{{ $i18n.t('dashboard.info.todayIncome') }} : </span>
+              <span style="font-size: 18px;font-weight: bold;">{{ dashboard.todayIncome }}</span>
+              <br>
+            </span>
+            <span>
+              <span>{{ $i18n.t('dashboard.info.todayOrderCount') }} : </span>
+              <span style="font-size: 18px;font-weight: bold;">{{ dashboard.todayOrderCount }}</span>
+              <br>
+            </span>
+            <span>
+              <span>{{ $i18n.t('dashboard.info.todayNewOrderCount') }} : </span>
+              <span style="font-size: 18px;font-weight: bold;">{{ dashboard.todayNewOrderCount }}</span>
+              <br>
+            </span>
           </a-card>
         </a-col>
-        <a-col :sm="24" :md="24" :lg="24" :xl="24" :style="{ marginBottom: '24px' }">
-          <a-card :title="$i18n.t('dashboard.info.log')" :bordered="false" style="height: 300px">
-            <p>TODO</p>
-          </a-card>
-        </a-col>
+        <div>
+          <a-button type="primary" @click="cleanCache">{{ $i18n.t('dashboard.cache.clean') }}</a-button>
+        </div>
       </div>
     </a-row>
-    <div>
-      <a-button type="primary" @click="cleanCache">{{ $i18n.t('dashboard.cache.clean') }}</a-button>
-    </div>
   </page-header-wrapper>
 </template>
 
