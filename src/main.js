d9a6ea4824342e01b49eefeed7bcf4d2086f0363
ByteInternetHK <byteinternethk@gmail.com>
2021-06-20 22:45:45 +0800

    markdown support video script

--- src/main.js
+++ src/main.js
@@ -26,6 +26,12 @@ import VMdEditor from '@kangc/v-md-editor'
 import '@kangc/v-md-editor/lib/style/base-editor.css'
 import vuepressTheme from '@kangc/v-md-editor/lib/theme/vuepress.js'
 VMdEditor.use(vuepressTheme)
+VMdEditor.xss.extend({
+  // extend white list
+  whiteList: {
+    source: ['src', 'type']
+  }
+})
 Vue.use(VMdEditor)
 
 const MyIcon = Icon.createFromIconfontCN({
