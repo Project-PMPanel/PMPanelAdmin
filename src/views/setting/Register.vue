db70ecf5b7cd54bf4fcb7f507bdee0354514f208
ByteInternetHK <byteinternethk@gmail.com>
2021-07-02 12:18:12 +0800

    remove user port

--- src/views/setting/Register.vue
+++ src/views/setting/Register.vue
@@ -10,14 +10,6 @@
             <a-input v-model="registerConfig.enableEmailSuffix" @blur="updateConfigByName('enableEmailSuffix', registerConfig.enableEmailSuffix)"/>
           </a-form-item>
 
-          <a-form-item :label="$t('settings.register.userPortRange')">
-            <a-input-group compact>
-              <a-input v-model="registerConfig.userMinPort" @blur="updateConfigByName('userMinPort', registerConfig.userMinPort)" style=" width: 100px; text-align: center" placeholder="Minimum" />
-              <a-input style=" width: 30px; border-left: 0; pointer-events: none; backgroundColor: #fff" placeholder="~" disabled/>
-              <a-input v-model="registerConfig.userMaxPort" @blur="updateConfigByName('userMaxPort', registerConfig.userMaxPort)" style="width: 100px; text-align: center; border-left: 0" placeholder="Maximum" />
-            </a-input-group>
-          </a-form-item>
-
           <a-form-item :label="$t('settings.register.inviteCount')">
             <a-input v-model="registerConfig.inviteCount" @blur="updateConfigByName('inviteCount', registerConfig.inviteCount)"/>
           </a-form-item>
@@ -67,16 +59,6 @@ export default {
   },
   methods: {
     async updateConfigByName (name, value) {
-      // 如果是用户端口号,拼接一下
-      if (name === 'userMinPort' || name === 'userMaxPort') {
-        // 判断范围是否规范
-        if (this.registerConfig.userMinPort >= this.registerConfig.userMaxPort || this.registerConfig.userMaxPort > 65535) {
-          this.$i18n.locale === 'zh-CN' ? this.$message.warn('请设置正确的端口范围,建议[10000:65535]') : this.$message.warn('please set up correct port range,suggest [10000:65535]')
-          return
-        }
-        name = 'userPortRange'
-        value = this.registerConfig.userMinPort.toString() + ':' + this.registerConfig.userMaxPort.toString()
-      }
       const params = {
         'name': name,
         'value': value
